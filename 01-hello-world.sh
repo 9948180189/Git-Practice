c3cefefdd1e506c75ec2d28413f98f15391baa75
9948180189 <hari10.143@gmail.com>
2025-01-13 13:05:57 +0530

    added some extrafeatures

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "hi this is my new project & Hi You Get Job Opertunity on MNC Company are you agree to Join"
+echo "hi this is my new project & Hi You Get Job Opertunity on MNC Company are you agree to Join & Yes Iam Verry much interested"

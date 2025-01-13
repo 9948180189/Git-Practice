f029430abeee0045c4a449fc2da219b58ca3980b
9948180189 <hari10.143@gmail.com>
2025-01-13 11:07:45 +0000

    some changes done

--- 03-variable.sh
+++ 03-variable.sh
@@ -2,4 +2,4 @@
 PERSON1=Ramesh
 PERSON2=Suresh
 echo "$PERSON1: Hi suresh, How Are you"
-echo "$PERSON2: yes iam good,HOW is Your Job $PERSON1"
+echo "${PERSON2}: yes iam good,HOW is Your Job $PERSON1"

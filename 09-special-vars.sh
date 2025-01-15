7a4e80a531c29cbea6cc0a9f819573a93c7ceca7
9948180189 <hari10.143@gmail.com>
2025-01-15 12:03:03 +0530

    modification on variables

--- 09-special-vars.sh
+++ 09-special-vars.sh
@@ -3,4 +3,5 @@ echo "All variables passed to the script: $*"
 echo "Number of veriables passed: $#"
 echo "Script name: $0"
 echo "Current Working diarectory: $PWD"
+echo "Home directory of current user: $HOME"
 

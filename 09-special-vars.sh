3840d0d57f9640ac8d85f0d815a36152a3d85c0b
9948180189 <hari10.143@gmail.com>
2025-01-15 12:08:27 +0530

    modification on variables

--- 09-special-vars.sh
+++ 09-special-vars.sh
@@ -5,4 +5,6 @@ echo "Script name: $0"
 echo "Current Working diarectory: $PWD"
 echo "Home directory of current user: $HOME"
 echo "PID of the script executing now: $$"
+sleep 100 &
+echo "PID of last background command: $!"
 

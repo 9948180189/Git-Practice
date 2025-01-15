40dc205a8e796d92df5054706c56a11a57afcfe9
9948180189 <hari10.143@gmail.com>
2025-01-15 12:41:17 +0530

    modification on variables

--- 10-Condition.sh
+++ 10-Condition.sh
@@ -4,9 +4,7 @@ NUMBER=$1
 
 if [ $NUMBER1 -gt 20 ] #gt, lt, eq, -ne, -ge, -le
 then
-    echo "Given number:$NUMBER is greater then 20"
-
+    echo "Given number: $NUMBER is greater then 20"
 else
-    echo "Given number:$NUMBER is less than then 20"
-
+    echo "Given number: $NUMBER is less than 20"
 fi       

8e61564660a5c40365b2bb083fd75ef0c7eea6f0
9948180189 <hari10.143@gmail.com>
2025-01-15 16:23:59 +0530

    code change in loop

---
+++ 15-Loops.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ] 
+    then
+        echo "Please run this script with root priveleges"
+        exit 1
+
+}
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is...$R FAILED $N"
+        exit 1
+    else
+        echo -e "$2 is...$G success $N"
+    fi
+}
+
+CHECK_ROOT
+for package in $@ 
+do
+    echo $package
+done

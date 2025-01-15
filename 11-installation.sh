b3674582dc5c23abed4029e3f23214449b299577
9948180189 <hari10.143@gmail.com>
2025-01-15 13:16:12 +0530

    modification on INSTALLATION

--- 11-installation.sh
+++ 11-installation.sh
@@ -7,6 +7,12 @@ then
     exit 1
 fi
 
-dnf install git -y
+dnf list installed git
+if [ $? -ne 0 ]
+then
+    echo "Git Is not installed, going to insall it.."
+    dnf install git -y
+else
+    echo "Git Already installed, nothing to do."
 
-dnf install mysql -y
+fi

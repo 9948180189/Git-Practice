d00a0f24cb2878df18faa31ad799ea7b231522d9
9948180189 <hari10.143@gmail.com>
2025-01-15 14:29:30 +0530

    modification on Functions

--- 12-functions.sh
+++ 12-functions.sh
@@ -2,7 +2,13 @@
 USERID=$(id -u)
 # echo "User ID is: $USERID"
 VALIDATE(){
-     echo "exit status: $1"
+    if [ $1 -ne 0 ]
+    then
+        echo "$2 is Faild'
+        exit  1
+    else
+        echo "$2 is .success"
+    fi    
 }
 if [ $USERID -ne 0 ]
 then
@@ -10,39 +16,39 @@ then
     exit 1
 fi
 
-dnf installed git
-VALIDATE $?
-if [ $? -ne 0 ]
-then
-    echo "Git Is not installed, going to insall it.."
-    dnf install git -y
-#     if [ $? -ne 0 ]
-#     then
-#         echo "Git installation is not success..check it"
-#         exit 1
-#     else
-#         echo "Git installation is Sucess"
-#     fi
-
-# else
-#     echo "Git Already installed, nothing to do."
-
-# fi
-# dnf installed mysql
-
+dnf list installed git
+VALIDATE $? "Listing Git"
 # if [ $? -ne 0 ]
 # then
-#     echo "Mysql is not installed..going to install"
-#     dnf install mysql -y
-#     if [ $? -ne 0 ]
-#     then
-#         echo "Mysql installation is failure..please check it."
-#         exit 1
-#     else 
-#         echo "Mysql installation is success"
+#     echo "Git Is not installed, going to insall it.."
+#     dnf install git -y
+# #     if [ $? -ne 0 ]
+# #     then
+# #         echo "Git installation is not success..check it"
+# #         exit 1
+# #     else
+# #         echo "Git installation is Sucess"
+# #     fi
 
-#     fi  
-# else
-#     echo "Mysql is Already Installed..nothind to do"
+# # else
+# #     echo "Git Already installed, nothing to do."
 
-# fi          
+# # fi
+# # dnf installed mysql
+
+# # if [ $? -ne 0 ]
+# # then
+# #     echo "Mysql is not installed..going to install"
+# #     dnf install mysql -y
+# #     if [ $? -ne 0 ]
+# #     then
+# #         echo "Mysql installation is failure..please check it."
+# #         exit 1
+# #     else 
+# #         echo "Mysql installation is success"
+
+# #     fi  
+# # else
+# #     echo "Mysql is Already Installed..nothind to do"
+
+# # fi          

f81fb213d890dd80e73208e687f1fc69b5017c8f
9948180189 <hari10.143@gmail.com>
2025-01-15 14:32:18 +0530

    modification on Functions

--- 12-functions.sh
+++ 12-functions.sh
@@ -18,37 +18,34 @@ fi
 
 dnf list installed git
 VALIDATE $? "Listing Git"
-# if [ $? -ne 0 ]
-# then
-#     echo "Git Is not installed, going to insall it.."
-#     dnf install git -y
-# #     if [ $? -ne 0 ]
-# #     then
-# #         echo "Git installation is not success..check it"
-# #         exit 1
-# #     else
-# #         echo "Git installation is Sucess"
-# #     fi
+if [ $? -ne 0 ]
+then
+    echo "Git Is not installed, going to insall it.."
+    dnf install git -y
+    if [ $? -ne 0 ]
+    then
+        echo "Git ins#  tallation is not success..check it"
+        exit 1
+    else
+        echo "Git installation is Sucess"
+    fi
 
-# # else
-# #     echo "Git Already installed, nothing to do."
+else
+    echo "Git Already installed, nothing to do."
 
-# # fi
-# # dnf installed mysql
-
-# # if [ $? -ne 0 ]
-# # then
-# #     echo "Mysql is not installed..going to install"
-# #     dnf install mysql -y
-# #     if [ $? -ne 0 ]
-# #     then
-# #         echo "Mysql installation is failure..please check it."
-# #         exit 1
-# #     else 
-# #         echo "Mysql installation is success"
-
-# #     fi  
-# # else
-# #     echo "Mysql is Already Installed..nothind to do"
-
-# # fi          
+fi
+dnf installed mysql
+if [ $? -ne 0 ]
+then
+    echo "Mysql is not installed..going to install"
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then
+        echo "Mysql installation is failure..please check it."
+        exit 1
+    else 
+        echo "Mysql installation is success"
+    fi  
+else
+    echo "Mysql is Already Installed..nothind to do"
+fi          

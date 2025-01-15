a5411c9b3bb8b0688db8815796bac68a2b384cbf
9948180189 <hari10.143@gmail.com>
2025-01-15 11:02:34 +0530

    modification on variables

--- 06-array.sh
+++ 06-array.sh
@@ -2,6 +2,6 @@
 FRUITS=("APPLE","KIWI","ORANGE")
 echo "First fruit is: ${FRUITS[0]}"
 echo "Second fruit is: ${FRUITS[1]}"
-echo "Third fruit is: ${FRUITS[2}"
+echo "Third fruit is: ${FRUITS[2]}"
 
 echo "All fruits are: ${FRUITS[@]}"

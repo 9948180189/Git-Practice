10b0606fd3d3940a3501f500b69b6ffb494acc6b
9948180189 <hari10.143@gmail.com>
2025-01-16 16:26:55 +0530

    Expense project from shell

--- 19-backup.sh
+++ 19-backup.sh
@@ -2,6 +2,7 @@
 SOURCE_DIR=$1
 DEST_DIR=$2
 DAYS=${3:-14}
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
@@ -31,6 +32,21 @@ echo "Files: $FILES"
 if [ ! -z $FILES ]
 then
     echo "Files are found"
+    ZIP_FILE="$DEST_DIR/app-logs-$TIMESTAMP.zip"
+    find ${SOURCE_DIR} -name "*.log" -mtime +14 | zip "$ZIP_FILE" -@
+    if [ -f $ZIP_FILE ]
+    then
+        echo "Sucessfully zipped files older than $DAY"
+        while IFS= read -r file
+        do
+            echo "Deliting file: $file"
+            rm -rf $file
+        done <<< $FILES
+
+    else
+        echo "Zipping the file is failed" 
+        exit 1   
+    fi
 else
     echo "No files older than $DAYS"
 fi        

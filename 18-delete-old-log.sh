6c476b63e441273ebda68776c4fc192e238e0341
9948180189 <hari10.143@gmail.com>
2025-01-16 14:06:31 +0530

    Expense project from shell

--- 18-delete-old-log.sh
+++ 18-delete-old-log.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 SOURCE_DIR=/home/ec2-user/logs
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
 
 if [ -d $SOURCE_DIR ]
 then
-    echo "$SOURCE_DIR Exists"
+    echo -e "$SOURCE_DIR $G Exists $N"
 else    
-    echo "$SOURCE_DIR does not exists"
+    echo -e "$SOURCE_DIR $R does not exists $N"
     exit 1
 fi        
